fba6747c41f97e657e036beda12a0f170059249c
italosaager <italosaagergmail.com>
2022-08-23 17:45:19 -0300

    Added some data json at src/assets/data. CoinItem component created with the needed props from data and displayed in a flatList.

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,31 +1,14 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image } from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
+import { StyleSheet, View, FlatList} from 'react-native';
+import CoinItem from '../Components/CoinItem/Index';
+import cryptocurrencies from '../Assets/data/cryptocurrencies.json'
 
 export default function Main() {
 
  return (
   <View style={styles.container}>
-    <View style={styles.cryptoContainer}>
-    <Image source={{uri: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579'}} style={styles.cryptoIcon} />
-
-        <View>
-          <Text style={styles.title}>Bitcoin</Text>
-          <View style={styles.coinData}>
-            <Text style={styles.text}>1</Text>
-            <Text style={styles.text}>BTC</Text>
-            <AntDesign name='caretup' size={16} color='green' style={{alignSelf: 'center'}} />
-            <Text style={styles.text}>%</Text>
-          </View>
-        </View> 
-
-        <View style={styles.coinData2}>
-            <Text style={styles.title}>123000</Text>
-            <Text style={styles.text}>Mcap btc</Text>
-          </View>
-        </View>
-
-
+    <FlatList data={cryptocurrencies} 
+    renderItem = {({item}) => <CoinItem marketCoin={item} /> } />
   </View>
   )
 }
@@ -36,32 +19,4 @@ const styles = StyleSheet.create({
     backgroundColor: '#121212',
     paddingTop: 50,
   },
-  title:{
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  text:{
-    color: '#fff',
-    marginRight: 5,
-  },
-  cryptoIcon:{
-    height: 35,
-    width: 35,
-    marginRight: 10,
-    alignSelf: 'center',
-  },
-  cryptoContainer:{
-    flexDirection: 'row',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    borderBottomColor: '#282828',
-    padding: 15,
-  },
-  coinData:{
-    flexDirection: 'row',
-  },
-  coinData2:{
-    marginLeft: 'auto',
-  }
-
 });
